7286893a1699b8b9b4e57bad429a4884c66156eb
Kevin <16568166+sirkevinc@users.noreply.github.com>
2018-03-09 13:40:48 -0500

    Added Statuses and Catch Errors

--- src/app.js
+++ src/app.js
@@ -9,6 +9,9 @@ const currentPriceUrl = 'https://api.coindesk.com/v1/bpi/currentprice.json';
 const yesterdayPriceUrl = 'https://api.coindesk.com/v1/bpi/historical/close.json?for=yesterday';
 const yesterdayDate = moment().add(-1, 'd').format("YYYY-MM-DD");
 
+const STATUS_SUCCESS = 200;
+const STATUS_ERROR = 422;
+
 app.get('/compare', (req, res) => {
   let currentPrice;
   let yesterdayPrice;
@@ -20,11 +23,22 @@ app.get('/compare', (req, res) => {
         .then(yesterdayPriceResponse => yesterdayPriceResponse.json())
         .then(yesterdayPriceResponse => {
           yesterdayPrice = yesterdayPriceResponse.bpi[yesterdayDate]
+          res.status(STATUS_SUCCESS);
           res.json({ CurrentPrice: currentPrice, 
             YesterdayPrice: yesterdayPrice, 
             PriceDifference: currentPrice - yesterdayPrice,
           })
         })
+        .catch(err => {
+          console.log(err);
+          res.status(STATUS_ERROR);
+          res.send({ err: err });
+        })
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(STATUS_ERROR);
+      res.send({ err: err });
     })
 })
 
