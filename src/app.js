3dc5601060bbc06b82aed646de7df297c1939435
Kevin <16568166+sirkevinc@users.noreply.github.com>
2018-03-09 12:53:12 -0500

    Imported Moment.js
    It appears that we will need yesterday's date in YYYY-MM-DD format
    in order to access yesterday's price.
    
    Created variable that will store yesterday's date in the correct format
    as a string so it can be used to access the value in that property
    within the yesterdayPrice json.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const fetch = require('node-fetch');
+const moment = require('moment');
 const PORT = 3030;
 
 const app = express();
 
 const currentPriceUrl = 'https://api.coindesk.com/v1/bpi/currentprice.json';
 const yesterdayPriceUrl = 'https://api.coindesk.com/v1/bpi/historical/close.json?for=yesterday';
+const yesterdayDate = moment().add(-1, 'd').format("YYYY-MM-DD");
 
 app.get('/compare', (req, res) => {
 })
